ab0af9ef91c220d7cfc23abd2acde3beb4f8e68b
neil <denck007@umn.edu>
2020-07-18 12:28:37 -0500

    Initial Commit

--- README.md
+++ README.md
@@ -1,2 +1,11 @@
 # 3dReconstruction
 Expoloring the world of generating 3d models from images
+
+## Homography.ipynb
+Homography assumes that all matched points are planar. Try to leverage this to stitch images together. The results are better than expected, but not great
+
+## StereoDepthMap.ipynb
+Use OpenCV to determine a depthmap for 2 images.
+
+## CameraPositions.ipynb
+Finding the essential or fundamental matrix determining the orientation of 2 images
---
+++ stereoParams.py
@@ -0,0 +1,154 @@
+
+import cv2
+import numpy as np
+import matplotlib.pyplot as plt
+import time
+
+
+def nothing(x):
+    pass
+def load_images():
+    scale = .25
+
+
+    image_left = cv2.imread("images/pineapple_left.jpg")
+    image_right = cv2.imread("images/pineapple_right.jpg")
+    image_left = cv2.resize(image_left,(0,0),fx=scale,fy=scale)
+    image_right = cv2.resize(image_right,(0,0),fx=scale,fy=scale)
+    return image_left, image_right
+
+def homography(image_left, image_right):
+
+    orb = cv2.ORB_create(nfeatures=1000)
+
+    kps1, descs1 = orb.detectAndCompute(left_gray,None)
+    kps2, descs2 = orb.detectAndCompute(right_gray,None)
+
+    MIN_MATCH_COUNT = 10
+    FLANN_INDEX_LSH = 6
+    index_params= dict(algorithm = FLANN_INDEX_LSH,
+                    table_number =12, # 12
+                    key_size = 10,     # 20
+                    multi_probe_level = 2) #2
+    search_params = {"checks":100}
+
+    flann = cv2.FlannBasedMatcher(index_params,search_params)
+    matches = flann.knnMatch(descs1, descs2, k=2)
+
+    # keep good matches
+    good = [m for m,n in matches if m.distance<0.7*n.distance]
+
+    if len(good) > MIN_MATCH_COUNT:
+        src_points = np.array([ kps2[m.trainIdx].pt for m in good], dtype=np.float32).reshape(-1,1,2)
+        dst_points = np.array([ kps1[m.queryIdx].pt for m in good], dtype=np.float32).reshape(-1,1,2)
+        
+        M, _ = cv2.findHomography(src_points, dst_points, cv2.RANSAC, 5.0)
+        image_right_homography = cv2.warpPerspective(image_right,M,(image_left.shape[1],image_left.shape[0]))
+    else:
+        print("Not enough matches found")
+        image_right_homography = None
+
+    return image_right_homography
+
+
+def compute_stereo(image_left,
+                    image_right,
+                    minDisparity=0,
+                    numDisparities=16,
+                    blockSize=3,
+                    P1=0,
+                    P2=0,
+                    disp12MaxDiff=0,
+                    preFilterCap=0,
+                    uniquenessRatio=0,
+                    speckleWindowSize=0,
+                    speckleRange=0):
+    start_time = time.time()
+    stereo = cv2.StereoSGBM_create(minDisparity,
+                                numDisparities,
+                                blockSize,
+                                P1,
+                                P2,
+                                disp12MaxDiff,
+                                preFilterCap,
+                                uniquenessRatio,
+                                speckleWindowSize,
+                                speckleRange)
+    create_time = time.time() - start_time
+
+    disparity = stereo.compute(image_left,
+                            image_right).astype(np.float32)
+    disparity = (disparity - disparity.min()) / (disparity.max() - disparity.min())
+    disparity = (255*disparity).astype(np.uint8)
+
+    compute_time = time.time() - start_time - create_time
+    elapsed_time = time.time() - start_time
+    fps = 1/ elapsed_time
+    print(f"Update time: {elapsed_time:.4f} create: {create_time:.6f} compute: {compute_time:.6f} fps: {fps:.1f}")
+
+    return disparity
+
+def update(_):
+    minDisparity = cv2.getTrackbarPos('minDisparity','image')
+    numDisparities = 16*cv2.getTrackbarPos('numDisparities','image')
+    blockSize = cv2.getTrackbarPos('blockSize','image')
+    if blockSize%2 == 0:
+        blockSize += 1
+        cv2.setTrackbarPos("blockSize","image",blockSize)
+    P1 = cv2.getTrackbarPos('P1','image')
+    P2 = cv2.getTrackbarPos('P2','image')
+    if P2 < P1:
+        P2 = P1+1
+        cv2.setTrackbarPos("P2","image",P2)
+    disp12MaxDiff = cv2.getTrackbarPos("disp12MaxDiff","image")
+    preFilterCap = cv2.getTrackbarPos("preFilterCap","image")
+    uniquenessRatio = cv2.getTrackbarPos("uniquenessRatio","image")
+    speckleWindowSize = cv2.getTrackbarPos("speckleWindowSize","image")
+    speckleRange = cv2.getTrackbarPos("speckleRange","image")
+    
+    disparity = compute_stereo(left_gray, 
+                                    right_homography_gray,
+                                    minDisparity=minDisparity,
+                                    numDisparities=numDisparities,
+                                    blockSize=blockSize,
+                                    P1=P1,
+                                    P2=P2,
+                                    disp12MaxDiff=disp12MaxDiff,
+                                    preFilterCap=preFilterCap,
+                                    uniquenessRatio=uniquenessRatio,
+                                    speckleWindowSize=speckleWindowSize,
+                                    speckleRange=speckleRange)
+    
+    
+    cv2.imshow('image',disparity)
+
+
+
+image_left, image_right = load_images()
+left_gray = cv2.cvtColor(image_left,cv2.COLOR_BGR2GRAY)
+right_gray = cv2.cvtColor(image_right,cv2.COLOR_BGR2GRAY)
+right_homography_gray = homography(left_gray, right_gray)
+
+disparity = compute_stereo(left_gray, right_homography_gray)
+
+cv2.namedWindow('image')
+cv2.createTrackbar('minDisparity','image',1,255,update)
+cv2.createTrackbar('numDisparities','image',1,20,update)
+cv2.createTrackbar('blockSize','image',1,21,update)
+cv2.createTrackbar('P1','image',0,255,update)
+cv2.createTrackbar('P2','image',0,255,update)
+cv2.createTrackbar('disp12MaxDiff','image',-1,255,update)
+cv2.createTrackbar('preFilterCap','image',0,255,update)
+cv2.createTrackbar('uniquenessRatio','image',0,255,update)
+cv2.createTrackbar('speckleWindowSize','image',0,255,update)
+cv2.createTrackbar('speckleRange','image',1,6,update)
+
+
+update(None)
+
+while True:
+    k = cv2.waitKey(0) & 0xFF
+    if k == 27:
+        break
+
+cv2.destroyAllWindows()
